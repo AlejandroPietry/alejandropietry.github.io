39c73018967e49a782b81e3723fe5a05310ea082
Alejandro Pietry <pietrytrindade12@gmail.com>
2021-03-31 18:41:46 -0300

    Verificando se tem algum valor nos campo de envio de mensagem

--- all.js
+++ all.js
@@ -2,9 +2,13 @@ function EnviarMensagemWpp(){
     var mensagem = document.getElementById("message").value;
     var nomePessoa = document.getElementById("fullname").value;
 
-    if(mensagem == null || nomePessoa == null){
+    console.log(mensagem)
+    console.log(nomePessoa)
+    if(mensagem == "" || nomePessoa == ""){
         return null
     }
+    else{
+        window.open("https://api.whatsapp.com/send?phone=5591991367966&text=" + mensagem + "att: " + nomePessoa);
+    }
 
-    window.open("https://api.whatsapp.com/send?phone=5591991367966&text=" + mensagem + "att: " + nomePessoa);
 }
